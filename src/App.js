e812af39748f0059a1abc6077ae7f51a12a9b544
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-04-09 16:31:40 -0300

    styles App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
+import './App.css';
+
 
 
 const App = () => {
     return (
         <div className='app'>
             <div className='navbar'>
-
+                
             </div>
             <div className='main'>
 

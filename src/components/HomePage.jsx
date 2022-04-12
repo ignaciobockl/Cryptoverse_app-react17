990ed72719062bf8a964fc42950ebe624fc67349
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-04-12 13:35:49 -0300

    HomePage.jsx in progress...

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,42 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
-
-import { useGetCryptosQuery } from '../services/cryptoApi';
+import { useSelector } from 'react-redux';
 
 
 const { Title } = Typography;
 
 const HomePage = () => {
 
-    const { data, isFetching } = useGetCryptosQuery();
-    const globalStats = data ?. data ?. stats;
+    const { status, data } = useSelector( state => state.crypto );
+    let stats;
 
-    if ( isFetching ) return 'Loading...';
+    if ( status === 'success') {
+        stats = data.stats;
+    }
 
     return (
         <>
             <Title level={ 2 } className='heading'>
                 Global Crypto Stats
             </Title>
-            <Row>
-                <Col span={ 12 }>
-                    <Statistic title='Total Cryptocurrencies' value={ globalStats.total } />     
-                </Col>
-                <Col span={ 12 }>
-                    <Statistic title='Total Exchanges' value={ millify( globalStats.totalExchanges ) } />     
-                </Col>
-                <Col span={ 12 }>
-                    <Statistic title='Total Market Cap' value={ millify( globalStats.totalMarketCap ) } />      
-                </Col>
-                <Col span={ 12 }>
-                    <Statistic title='Total 24h Volume' value={ millify( globalStats.total24hVolume ) } />     
-                </Col>
-                <Col span={ 12 }>
-                    <Statistic title='Total Markets' value={ millify( globalStats.totalMarkets ) } />     
-                </Col>
-            </Row>
+            {
+                stats !== undefined &&
+                <Row>
+                    <Col span={ 12 }>
+                        <Statistic title='Total Cryptocurrencies' value={ stats.total }/>    
+                    </Col>
+                    <Col span={ 12 }>
+                        <Statistic title='Total Exchanges' value={ millify( stats.totalExchanges ) } />     
+                    </Col>
+                    <Col span={ 12 }>
+                        <Statistic title='Total Market Cap' value={ millify( stats.totalMarketCap ) } />      
+                    </Col>
+                    <Col span={ 12 }>
+                        <Statistic title='Total 24h Volume' value={ millify( stats.total24hVolume ) } />     
+                    </Col>
+                    <Col span={ 12 }>
+                        <Statistic title='Total Markets' value={ millify( stats.totalMarkets ) } />     
+                    </Col>
+                </Row>
+            }
+            
             {/* 3:42:43 */}
         </>
     )

f73722bbc6382221d58ca9f45a6d40fb5ce7d63d
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-04-15 20:12:41 -0300

    CryptoDetails.jsx in progress

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import HTMLReactParser from 'html-react-parser';
 import { useParams } from 'react-router-dom';
 import millify from 'millify';
 import { Col, Row, Typography, Select } from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined } from '@ant-design/icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCrypto } from '../actions/crypto';
 
 
 const { Title, Text } = Typography;
@@ -13,8 +15,21 @@ const CryptoDetails = () => {
 
     const { coinId } = useParams();
 
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch( getCrypto( coinId ) );
+    }, []);
+    
+
+    const { coinSelected } = useSelector( state => state.crypto );
+    const { data, status } = coinSelected;
+    console.log('state', data);
+
     const [ timePeriod, setTimePeriod ] = useState('7d');
     // 4:21:15
+
+    
 
     return (
         <div>CryptoDetails {coinId}</div>

9ec6a20b576e5f6f16d5a4f2a8901a6574d36d39
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-04-15 19:17:51 -0300

    CryptoDetails.jsx in progress

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import HTMLReactParser from 'html-react-parser';
+import { useParams } from 'react-router-dom';
+import millify from 'millify';
+import { Col, Row, Typography, Select } from 'antd';
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined } from '@ant-design/icons';
 
 
+const { Title, Text } = Typography;
+const { Option } = Select;
 
 const CryptoDetails = () => {
 
+    const { coinId } = useParams();
+
+    const [ timePeriod, setTimePeriod ] = useState('7d');
     // 4:21:15
 
     return (
-        <div>CryptoDetails</div>
+        <div>CryptoDetails {coinId}</div>
     )
 }
 

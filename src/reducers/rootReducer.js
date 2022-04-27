3660e3858365904eb799bda2e628e7fb5fcbe11e
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-04-27 20:15:59 -0300

    the exchange reducer was removed because the endpoint does not work in the free version

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,12 +2,10 @@ import { combineReducers } from "redux";
 
 import { bingNewsReducer } from "./bingNewsReducer";
 import { cryptoReducer } from "./cryptoReducer";
-import { exchangeReducer } from "./exchangeReducer";
 
 
 
 export const rootReducer = combineReducers({
     bingNews: bingNewsReducer,
     crypto: cryptoReducer,
-    exchange: exchangeReducer
 });
